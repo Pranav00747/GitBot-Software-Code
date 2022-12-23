1260fe843a214f361a9e43a668d874f7a410ea85
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2022-12-24 02:33:52 +0530

    Update funny_something.go

--- funny_something.go
+++ funny_something.go
@@ -5,6 +5,6 @@ func somerhing_funny_(_ques string, _ans string)
   cl.insertOne(bson.D{{"Question":_ques, "Answere":_ans}})
 }
 
-something_funny_("What If I have scored between 90-100 then, What Next ?", "Watch Prisonor of Azkaban Movie Again")
+something_funny_("Dad What If I have scored between 90-100 then, What Next ?", "Watch Prisonor of Azkaban Movie Again")
    
   
