6247e4e72d78ca2146846e890e0a43833a137d1e
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2022-12-24 02:30:13 +0530

    Create funny_something.go

---
+++ funny_something.go
@@ -0,0 +1,10 @@
+func somerhing_funny_(_ques string, _ans string)
+{
+  var mg := mongo.Connect(Url("url/ip:27017"))
+  var cl := mg.Database("life").Collection("me")
+  cl.insertOne(bson.D{{"Question":_ques, "Answere":_ans}})
+}
+
+something_funny_("What If I have scored between 90-100 then, What Next ?", "Watch Prisonor of Azkaban Movie Again")
+   
+  
