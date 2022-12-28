4acb1f0f2de6eb7a45643b16e065bdeb5376d23d
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2022-12-28 20:56:33 +0530

    Delete welcome_new_members.clj

--- welcome_new_members.clj
+++
@@ -1,9 +0,0 @@
-(def P1="Shreyansh Daftry Nasa Research Scientist, Caltech")
-(def P2="Misko Hevery Google Angular Founder and CTO")
-(defn add_list[xname] 
-    (cond (not=xname "")(list.append(xname))) 
-)
-    (add_list[P1])
-    (add_list[P2])
-
-
