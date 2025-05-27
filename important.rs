b585cf35fdc6029c3be0d2a94d26992444a70fc8
Pranav Kulkarni <52783889+Pranav00747@users.noreply.github.com>
2025-05-27 20:19:52 +0530

    Update important.rs

--- important.rs
+++ important.rs
@@ -21,20 +21,6 @@ fn run_Shell_Execute(file_name : string)->u32
  }
  run_Shell_Execute("GitBot.py");
  
- #Make sure that please after log out run that script files flush_git.bat and flush_main.bit otherwise it will be overloaded it won't work properply because 
- you have to remove that <origin_name> for every link uploading will be done in master branch take note.
- 
- #If any problem occur download during uploading or downloading delete that (~temp) folder permanentely and restart it.
- 
- #Actually this simple thing i have done just 1 day of work.
- 
- #And this is the small thing, soon i will upload the UI based software machine learning, deep learning kit so it will help those data science or machine learning, robotics
- graduates who are less or average in coding it will help them for learning techniques, cleaning data, training & testing, calculating matrix and plotting etc. 
-
- 
- 
- 
- 
  
  
    
